d982de9ff9a64bec6bb202868ac54ecdb14e8746
olana <olikorma@gmaill.com>
2022-06-03 15:46:58 -0700

    some stuff

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/**
+ *  * main - Prints the lowercase alphabet in reverse.
+ *
+ *   *
+ * *    * Return: Always 0.
+ *
+ **/
+
+int main(void)
+
+/**some comment**/
+
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	{
+	putchar(l);
+	putchar('\n');
+	}
+	return (0);
+}
