67efde836cbe1f8019b947c9850103c3d28cf69d
olana <olikorma@gmaill.com>
2022-06-04 10:03:40 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  *
- *
  * Description: print the statement
  *
  * Return: Always 0 (success)
- */ 
-int main(void)
+ */
+	int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
