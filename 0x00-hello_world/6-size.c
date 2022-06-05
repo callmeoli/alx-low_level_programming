5e87dba0f4b26fa700ef71733ab774a1b8a3895f
olana <olikorma@gmaill.com>
2022-06-05 02:11:44 -0700

    some stuff

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	printf("Size of an int is: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int is: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int is: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
 		return (0);
 }
