332bb1f146977fca5bf93cd798bafbcdf5cfa925
olana <olikorma@gmaill.com>
2022-06-08 01:21:52 -0700

    created _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+/**
+*Description: print putchar
+*File: some comment
+*/
+#include <unistd.h>
+#include "_putchar.c"
+/**
+ * Description: print putchar
+ * Main - Prints "_putchar" followed by new line.
+ *
+ * return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower -return 1 if lowercase alphabet and 0 other wise
+*@c: takes in a character
+*Return:0 for uppercase, 1 for lowercase
+*/
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}
+
