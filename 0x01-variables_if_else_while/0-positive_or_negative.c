dd53ac181e64b2f66e90ceda7cbbc85dc3fcefc3
olana <olikorma@gmaill.com>
2022-06-10 06:25:02 -0700

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,32 @@
-#include <stdio.h>
-#include <time.h>
-/* more header goes there */
 #include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 /**
- * main - return alphabets expect e and q
- *
- * Description: print the required results
- *
- * Return: integer value 0
- *
- */
-	int main(void)
-	{
+*  main -> this is a function to print
+*  Return: success always 0
+*/
+/* betty style doc for function main goes there */
+int main(void)
+{
 	int n;
 
 	srand(time(0));
-
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is postive\n", n);
-	} else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	} else
-	{
-		printf("%d is negative\n", n);
-	}
+	if (n < 0)
+		{
+		printf("%d is negative", n);
+		}
+	else if (n == 0)
+		{
+		printf("%d is zero", n);
 
-		return (0);
-	}
+		}
+	else
+		{
+		printf("%d is postive", n);
+		}
+		printf("\n");
+	return (0);
+}
