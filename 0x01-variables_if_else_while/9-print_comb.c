a6849a58d01570ca614056fa2b4fef50718b3109
olana <olikorma@gmaill.com>
2022-06-11 02:05:49 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
 /**
- *  * main - main Function
+ * main - entry block
  *
- *   *
- *
- *    * Return: 0
- *
+ * Return: 0
  **/
-
 int main(void)
-/*bullshiting*/
 {
-	int i = 0;
+	int i;
 
-	for (i = 10; i < 99; i++)
+	for (i = 0; i < 10; i++)
 	{
-	putchar(i);
-	putchar(',');
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
