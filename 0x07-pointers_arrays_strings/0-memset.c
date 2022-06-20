fe02415bb7f3ceee49ec6820662d8c503f3e0f34
olana <olikorma@gmaill.com>
2022-06-20 14:22:24 -0700

    hot fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 #include <stdio.h>
-/*
-* *_memset - set char to assigned value
+/**
+* _memset - sets memory to array
 * @s: buffer character
 * @b: assigned value
 * @n: number of char to be assined
-* 
 * Return: 0 Always sucsses
 */
-char *_memset(char *s, char b, unsigned int n) 
+char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
-  
-  for( i = 0;i < n;i++ )
-    {
-      s[i] = b;
-    }
-  return 0;
+	unsigned int i;
+
+for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+
+return (0);
 }
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+on pointer and arrays
