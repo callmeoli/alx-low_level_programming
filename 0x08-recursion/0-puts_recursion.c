19f3593a05e7003c25754a1c3819e7794f93ef09
olana <olikorma@gmaill.com>
2022-06-22 16:06:00 -0700

    hot fic

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * _puts_recursion - print string
+* s - input string
 * @s - input string
 * @i - count variable
 */
@@ -9,8 +10,9 @@ void _puts_recursion(char *s)
 int i = 0;
 	_putchar(s[i]);
 	i++;
-	if(s[i] != '\0')
+	if (s[i] != '\0')
 	{
-	_puts_recursion(s+i);
+	_puts_recursion(s + i);
 	}
+	_putchar('\n');
 }
