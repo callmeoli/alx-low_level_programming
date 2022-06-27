aaf6282d5ca004c126750d292599e1634a7eb292
olana <olikorma@gmaill.com>
2022-06-27 02:52:34 -0700

    hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-
+	(void)argc;
 	return (0);
 }
