446237593dff8f242c84189e873c4b8113fc460d
olana <olikorma@gmaill.com>
2022-06-28 13:02:59 -0700

    Frist commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*
+* create_array - creat array with specific size and and intial
+* @size: size of array
+* @c: intial character
+* Return - null if fail ptr if success
+*/
+ 
+char *create_array(unsigned int size, char c) {
+    char *ptr;
+    ptr = malloc(sizeof(char) * size);
+    if (size == 0){
+        return (NULL);
+    }else if (ptr == NULL){
+    
+        return (NULL);
+    }else{
+        int i;
+        for (i = 0; i < size; i++){
+        ptr[i] = 'H'; 
+        }
+     return (ptr);   
+    } 
+}
