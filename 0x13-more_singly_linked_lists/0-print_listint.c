62cdad2b7589f6194b4fd49b242effcad0c57269
olana <olikorma@gmaill.com>
2022-07-18 09:45:40 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+* print_listint - prints linked list
+* @h: head of the list
+* return: number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{	
+		count++;	
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
