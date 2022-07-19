381a3538a23d8004a9c99c62cd5a1117d242a0fc
olana <olikorma@gmaill.com>
2022-07-19 06:19:19 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,12 +8,21 @@
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int pos = 0;
+unsigned int pos = 0, count = 0;
+listint_t *tmp;
 
 	if (head == NULL)
 	{
 	return (NULL);
 	}
+	tmp = head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		count++;
+	}
+	if (index > count)
+		return (NULL);
 	while (pos < index)
 	{
 	pos++;
