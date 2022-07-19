ee17ca99d4d05444539632a4a9db0b5a1d2cb982
olana <olikorma@gmaill.com>
2022-07-19 08:15:28 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 		count++;
 	}
-	if (idx > (count + 1))
+	if (idx > count)
 	{
 		return (NULL);
 	}
