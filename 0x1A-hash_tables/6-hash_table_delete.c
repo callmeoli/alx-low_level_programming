d4ca8c4d0fb9e19413adacefc5af6f857bc2c7c6
Callmeoli <olikorma@gmail.com>
2022-09-24 18:09:58 +0300

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete hash table
+ * @ht: head of hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
+	unsigned long int i = 0
+	
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp1 = ht->array[i];
+		while((tmp2 = tmp1) != NULL)
+		{
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
+		}
+
+	}
+	free(ht->array);
+	free(ht);
+}
