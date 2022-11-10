0d5b467294ea0bdc8c3db86f19f15809920655ee
Callmeoli <olikorma@gmail.com>
2022-11-10 18:17:05 +0300

    all

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,10 +5,16 @@
 def island_perimeter(grid):
     """ claculate the area of """
     count = 0
+    x = 0
+    y = 0
+    postio=[0, 0]
     for i in grid:
+        x= x + 1
         for j in i:
+            y = y + 1
             if j == 1:
                 count = count + 1
+
     if count > 2:
         PI = (count - 2) * 2
         PO = 2 * 3
