63951cad1a29d1b7854c5cf4a88fc840745e98e1
allmeoli <olikorma@gmail.com>
2023-01-04 14:18:45 +0300

    jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <math.h>
 /**
  * jump_search - Entry point
  * @array: input array
@@ -8,16 +9,42 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int i;
+	int sz = (int)size;
+	int i, start = 0, end = (int)sqrt(sz);
 
-	for (i = 0; i < (int)size; i++)
+	if (array == NULL)
 	{
+		return (-1);
+	}
+	printf("Value checked array[%d] = [%d]\n", start, array[start]);
+	if (array[start] == value)
+	{
+		return (start);
+	}
+	printf("Value checked array[%d] = [%d]\n", end, array[end]);
+	if (array[end] == value)
+	{
+		return (end);
+	}
+
+	while (array[end] < value && start < sz)
+	{
+		start = end;
+		end = end + (int)sqrt(sz);
+			if (end > sz - 1)
+			{
+				end = sz;
+			}
+	}
+	printf("Value found between indexes [%d] and [%d]\n", start, end);
+	for (i = start; i < end; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
 			return (i);
 		}
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 	}
+
 	return (-1);
 }
